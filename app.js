b9048ff28f22475f51effc8e4e9ba035da73ae98
Rifqi <rief.rosyidi@gmail.com>
2019-05-10 22:37:58 +0700

    add bower components

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // DB Authentication
 // mongoose.connect("mongodb://username:password@localhost/dbused?authSource=dbauth&w=1");
-mongoose.connect("mongodb://rief:noPassword@localhost/nodekb?authSource=admin&w=1");
+mongoose.connect("mongodb://rief:noPassword@localhost/nodekb?authSource=admin&w=1", { useNewUrlParser: true } );
 let db = mongoose.connection;
 
 
@@ -26,6 +27,16 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Bosy Parser Middleware parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+// Set Public Folder Static
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Home Route
 app.get('/', function(req, res) {
   Article.find({}, function(err, articles) {
@@ -45,6 +56,18 @@ app.get('/articles/add', function(req, res) {
   res.render('add_article', {
     title:'Add Articles'
   });
+});
+
+// Add Submit POST Route
+app.post('/articles/add', function(req, res) {
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save(function(){
+      res.redirect('/');
+  })
 });
 
 // Start Server
