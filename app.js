ea3437188cc00812990dafbbfb96b7bc44c434a9
Rifqi <rief.rosyidi@gmail.com>
2019-05-10 23:19:20 +0700

    add link to individual article

--- app.js
+++ app.js
@@ -51,6 +51,15 @@ app.get('/', function(req, res) {
   });
 });
 
+// Get a Single Article
+app.get('/article/:id', function(req, res) {
+  Article.findById(req.params.id, function(err, article){
+    res.render('article', {
+      article:article
+    });
+  })
+})
+
 // Add Route
 app.get('/articles/add', function(req, res) {
   res.render('add_article', {
